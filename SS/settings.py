900620327b7e7cd1ba6cf8b9c5170184ee07e495
SandeshKutwade <40520057+sandeshkutwade@users.noreply.github.com>
2022-01-18 16:57:02 +0530

    Update settings.py
    changing security key in settings.py

--- SS/settings.py
+++ SS/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5559ii5sea1p)vva34pmv(a98czell-qhg6+1-o3)a)@jn6g#d'
+SECRET_KEY = 'abc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
